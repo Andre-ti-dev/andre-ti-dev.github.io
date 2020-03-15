00bba5af439487f474ad8978f3cf112cd1eac46e
Andre-ti-dev <andreluiz.ti.dev@gmail.com>
2020-03-15 19:58:40 -0300

    Added sjf not preemptive and preemptive simulation

--- js/main.js
+++ js/main.js
@@ -6,30 +6,166 @@ setTimeout(
 let processes = document.querySelector('#processes');
 let browsers = [];
 
-function sort() {
+async function sort() {
   let browsersSorted = [...browsers].sort((a, b) => (a.ms > b.ms ? 1 : -1));
   let browserIndex = 0;
-  let changePositions = setInterval(() => {
-    if (browsers.length > browserIndex) {
-      animationChangePosition(
-        getProcess(browsersSorted[browserIndex].id, browsers[browserIndex].id)
-      );
-      browserIndex++;
-    } else {
-      clearInterval(changePositions);
-    }
-  }, 1000);
+  while (browsers.length > browserIndex) {
+    await animationChangePosition(
+      getProcess(
+        browsersSorted[browserIndex].id,
+        getProcess(browsers[browserIndex].id)
+      )
+    );
+    browserIndex++;
+  }
+  browsers = browsersSorted;
+  return [...browsers];
 }
 
 function animationChangePosition(element, beforeElement) {
   element.style.opacity = 0;
-  setTimeout(function() {
-    processes.insertBefore(element, beforeElement);
+  return new Promise(resolve => {
     setTimeout(function() {
-      element.style.opacity = 1;
-    }, 1000);
-  }, 2000);
+      processes.insertBefore(element, beforeElement);
+      setTimeout(function() {
+        element.style.opacity = 1;
+        resolve();
+      }, 500);
+    }, 500);
+  });
 }
+
+async function fifo() {
+  disabledMethodButtons();
+  if (browsers.length > 0) {
+    startAnimations();
+    while (browsers.length > 0) {
+      await runProcessNotPreemptive(browsers[0]);
+      let browser = browsers.shift();
+      removeProcess(browser.id);
+      await contextChange();
+    }
+    stopAnimations();
+  }
+  enableMethodButtons();
+}
+
+async function sjfNotPreemptive() {
+  disabledMethodButtons();
+
+  if (browsers.length > 0) {
+    startAnimations();
+    while (browsers.length > 0) {
+      let browser = browsers.reduce((min, next) =>
+        next.ms < min.ms ? next : min
+      );
+
+      await runProcessNotPreemptive(browser);
+
+      browsers = browsers.filter(b => b.id != browser.id);
+      removeProcess(browser.id);
+      await contextChange();
+    }
+
+    stopAnimations();
+  }
+  enableMethodButtons();
+}
+
+async function sjfNotPreemptive() {
+  disabledMethodButtons();
+
+  if (browsers.length > 0) {
+    startAnimations();
+    while (browsers.length > 0) {
+      let browser = browsers.reduce((min, next) =>
+        next.ms < min.ms ? next : min
+      );
+
+      await runProcessNotPreemptive(browser);
+
+      browsers = browsers.filter(b => b.id != browser.id);
+      removeProcess(browser.id);
+      await contextChange();
+    }
+
+    stopAnimations();
+  }
+  enableMethodButtons();
+}
+
+async function sjfPreemptive() {
+  disabledMethodButtons();
+
+  if (browsers.length > 0) {
+    startAnimations();
+    let lastBrowser;
+
+    while (browsers.length > 0) {
+      let browser = browsers.reduce((min, next) =>
+        next.ms < min.ms ? next : min
+      );
+
+      if (!lastBrowser) switchProcessIcon(browser.icon);
+      if (lastBrowser && lastBrowser.id != browser.id) {
+        await contextChange();
+        switchProcessIcon(browser.icon);
+      }
+
+      await runProcessPreemptive(browser);
+
+      if (browser.ms == 0) {
+        browsers = browsers.filter(b => b.id != browser.id);
+        removeProcess(browser.id);
+      }
+
+      lastBrowser = Object.assign({}, browser);
+    }
+
+    stopAnimations();
+  }
+  enableMethodButtons();
+}
+
+function runProcessNotPreemptive(browser) {
+  switchProcessIcon(browser.icon);
+  return new Promise(resolve => {
+    let interval = setInterval(() => {
+      browser.ms--;
+      getProcess(browser.id).querySelector('.process-time').textContent =
+        browser.ms + 'ms';
+      if (browser.ms == 0) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 1000);
+  });
+}
+
+function runProcessPreemptive(browser) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      browser.ms--;
+      getProcess(browser.id).querySelector('.process-time').textContent =
+        browser.ms + 'ms';
+      resolve();
+    }, 1000);
+  });
+}
+
+function contextChange() {
+  let iconElement = document.createElement('i');
+  iconElement.classList.add('fas', 'fa-cog');
+
+  removeProcessIcon().appendChild(iconElement);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+}
+
+//
 
 function getProcess(id) {
   return document.querySelector("[data-id='" + id + "']");
@@ -39,6 +175,7 @@ function addProcess(event) {
   let icon = event.dataset.icon;
   let ms = event.dataset.ms;
   let id = browsers.length > 0 ? browsers[browsers.length - 1].id : 0;
+  id++;
 
   let processElement = document.createElement('div');
   processElement.classList.add('process');
@@ -59,55 +196,10 @@ function addProcess(event) {
   browsers.push({ id, ms, icon });
 }
 
-async function fifo() {
-  disabledMethodButtons();
-  if (browsers.length > 0) {
-    startAnimations();
-    while (browsers.length > 0) {
-      await runProcess(browsers[0]);
-      let browser = browsers.shift();
-      removeProcess(browser.id);
-      await contextChange();
-    }
-    stopAnimations();
-  }
-  enableMethodButtons();
-}
-
-function runProcess(browser) {
-  switchProcessIcon(browser.icon);
-  return new Promise(resolve => {
-    let interval = setInterval(() => {
-      browser.ms--;
-      if (browser.ms == 0) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, 1000);
-  });
-}
-
-function contextChange() {
-  let iconProcess = document.querySelector('#icon-process');
-  let iconElement = document.createElement('i');
-  iconElement.classList.add('fas', 'fa-cog');
-
-  iconProcess.removeChild(iconProcess.firstChild);
-  iconProcess.appendChild(iconElement);
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-}
-
 function switchProcessIcon(icon) {
-  let iconProcess = document.querySelector('#icon-process');
-
   let iconElement = document.createElement('i');
   iconElement.classList.add('fab', icon);
-  if (iconProcess.firstChild) iconProcess.removeChild(iconProcess.firstChild);
-  iconProcess.appendChild(iconElement);
+  removeProcessIcon().appendChild(iconElement);
 
   return icon;
 }
@@ -128,10 +220,13 @@ function stopAnimations() {
   loader.style.animation = '';
   loader.style.opacity = 0;
   document.querySelector('#cpu-wrapper').style.animation = '';
+  removeProcessIcon();
+}
+function removeProcessIcon() {
   let iconProcess = document.querySelector('#icon-process');
   if (iconProcess.firstChild) iconProcess.removeChild(iconProcess.firstChild);
+  return iconProcess;
 }
-
 function removeProcess(id) {
   let process = getProcess(id);
   processes.removeChild(process);
